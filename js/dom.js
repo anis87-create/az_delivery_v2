5c6d4d900ca720e42ac375d163a0446d5409513f
ZARROUK Anis <anis.zarrouk@witik.io>
2025-07-20 11:42:32 +0100

    fix bug of items calcul

--- js/dom.js
+++ js/dom.js
@@ -7,35 +7,19 @@ export const setupAddButtons = () => {
       itemButton.style.display = 'none';
       var div = document.createElement('div');
       var buttonPlus = document.createElement('button');
-      buttonPlus.textContent='+';
-      buttonPlus.style.background= '#f9fafb';
-      buttonPlus.style.boxShadow='rgba(0, 0, 0, 0.16) 0px 1px 4px;'
-      buttonPlus.style.borderRadius='50%';
-      buttonPlus.style.width= '2rem';
-      buttonPlus.style.height= '2rem';
-      buttonPlus.style.color='#333';
-      buttonPlus.style.borderColor='#ccc';
-      buttonPlus.style.borderWidth='1.5px';
-      buttonPlus.style.borderStyle='solid';
-      buttonPlus.style.marginLeft ='0.313rem';
+      buttonPlus.style.marginLeft='0.313rem';
+      buttonPlus.textContent = '+';
+      UIButton(buttonPlus);
+   
       var itemsNumber = document.createElement('span');
       var number = document.createTextNode('1');
       itemsNumber.appendChild(number);
       itemsNumber.style.fontSize='1.25rem';
       itemsNumber.style.fontWeight='500';
       var buttonMinus = document.createElement('button');
-      buttonMinus.textContent = '-';
-      buttonMinus.style.background= '#f9fafb';
-      buttonMinus.style.boxShadow='rgba(0, 0, 0, 0.16) 0px 1px 4px;'
-      buttonMinus.style.borderRadius='50%';
-      buttonMinus.style.width= '2rem';
-      buttonMinus.style.height= '2rem';
-      buttonMinus.style.color='#333';
-      buttonMinus.style.borderColor='#ccc';
-      buttonMinus.style.borderWidth='1.5px';
-      buttonMinus.style.borderStyle='solid';
       buttonMinus.style.marginRight='0.313rem';
-     
+      buttonMinus.textContent='-';
+      UIButton(buttonMinus);
       div.appendChild(buttonMinus);
       div.appendChild(itemsNumber);
       div.appendChild(buttonPlus);
@@ -43,23 +27,68 @@ export const setupAddButtons = () => {
       const name = item.children[0].textContent;
       const ingredients = item.children[item.children[0].nextElementSibling.tagName==='SPAN' ? 2:1].textContent;
       const price =  Number(item.children[item.children[0].nextElementSibling.tagName==='SPAN' ? 3:2].children[0].textContent.split('')[1]);
-      
-      addToCart(items, {id: ++id, name, ingredients, price});      
+      addToCart(items, {id: ++id,quantity: 1, name, ingredients, price});  
+      buttonPlus.addEventListener('click', (e) => {
+         e.preventDefault();
+         let numberOfItems = Number(e.target.previousElementSibling.textContent);
+
+         numberOfItems++;
+         items = JSON.parse(localStorage.getItem('items')) || [];
+         e.target.previousElementSibling.textContent = numberOfItems;
+    
+         const index = items.findIndex(item => item.name === name);         
+         if (index !== -1) {
+            items[index].quantity = numberOfItems;
+         }
+
+         localStorage.setItem('items', JSON.stringify(items));
+      });
+
+
+
+   
       e.target.parentElement.appendChild(div);
-       buttonMinus.addEventListener('click', (e) => {
-        div.style.display='none';
-        itemButton.style.display = 'block';
-        const itemName = e.target.parentElement.parentElement.parentElement.children[0].textContent;
-        const itemsParsed = JSON.parse(localStorage.getItem('items'));
-        const foundedItemById = itemsParsed.find(item  => item.name === itemName).id;
-        const updatedItems = itemsParsed.filter(item => item.id !== foundedItemById);
-        localStorage.setItem('items', JSON.stringify(updatedItems));    
-      })
-      
+      buttonMinus.addEventListener('click', (e) => {       
+         e.preventDefault();
+         let numberOfItems = Number(e.target.nextElementSibling.textContent);
+         numberOfItems--;
+         items = JSON.parse(localStorage.getItem('items')) || [];
+         e.target.nextElementSibling.textContent = numberOfItems;  
+         const index = items.findIndex(item => item.name === name);
+         if (index !== -1) {
+            items[index].quantity = numberOfItems;
+         }
+         localStorage.setItem('items', JSON.stringify(items));
+        if(numberOfItems === 0){
+            div.style.display='none';
+            itemButton.style.display = 'block';
+            const itemName = e.target.parentElement.parentElement.parentElement.children[0].textContent;
+            removeFromCart(localStorage.getItem('items'), itemName);       
+        }         
+      });
+     
    }))
 }
 
 const addToCart = (arr, item) => {
   arr.push(item);
   localStorage.setItem('items', JSON.stringify(arr));
+  localStorage.setItem('numberOfItems', 0);
+}
+const removeFromCart = (arr, itemName) => {
+  const itemsParsed = JSON.parse(arr);
+  const foundedItemById = itemsParsed.find(item  => item.name === itemName)?.id;
+  const updatedItems = itemsParsed.filter(item => item?.id !== foundedItemById);
+  localStorage.setItem('items', JSON.stringify(updatedItems));  
+}
+const UIButton = (button) => {
+   button.style.background= '#f9fafb';
+   button.style.boxShadow='rgba(0, 0, 0, 0.16) 0px 1px 4px;'
+   button.style.borderRadius='50%';
+   button.style.width= '2rem';
+   button.style.height= '2rem';
+   button.style.color='#333';
+   button.style.borderColor='#ccc';
+   button.style.borderWidth='1.5px';
+   button.style.borderStyle='solid';
 }
